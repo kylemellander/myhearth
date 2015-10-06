3cf5cc4fd810fe71779a479aee4a4bb6fdf2ca41
Kyle Mellander <kylemellander@gmail.com>
2015-10-05 19:51:49 -0700

    Add reset of cards if you get beyond 2

--- app/routes/edit-collection.js
+++ app/routes/edit-collection.js
@@ -6,31 +6,31 @@ export default Ember.Route.extend({
       cards: this.store.findAll('card'),
       join: this.store.find('carduser', {user: this.get('session').get('user')})
     });
-    return ;
   },
   actions: {
     addCard(card, user, join) {
       var found = false;
+      var newCardUser;
       join.forEach(function(joinItem) {
         if(joinItem.get('card').get('id') === card.id) {
-          var newCardUser = joinItem;
+          newCardUser = joinItem;
           var newCount = newCardUser.get('count') + 1;
           newCardUser.set('count', newCount);
           found = true;
         }
-      })
+      });
       if (found === false) {
         var cardUserParams = {card: card, user: user, count: 1};
-        var newCardUser = this.store.createRecord('carduser', cardUserParams);
+        newCardUser = this.store.createRecord('carduser', cardUserParams);
       }
-      if (newCardUser.get('count') < 2) {
+      if (newCardUser.get('count') < 3 && newCardUser.get('count') > 0) {
         newCardUser.save().then(function() {
           card.get('card_users').addObject(newCardUser);
           user.get('card_users').addObject(newCardUser);
           card.save().then(function() {
             user.save();
-          })
-        })
+          });
+        });
       } else {
         newCardUser.destroyRecord();
       }
