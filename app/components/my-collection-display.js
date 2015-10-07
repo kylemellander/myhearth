c8d0d5856f752fbd9cfe62b31de4efb27b4f756d
Christoph Pirringer & Lisa Duschek <lisa.duschek@hotmail.com>
2015-10-07 16:44:53 -0700

    add styling, background

--- app/components/my-collection-display.js
+++ app/components/my-collection-display.js
@@ -1,9 +1,37 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortedCardsOrder: ['cost', 'type:desc', 'name'],
+  sortedCards: Ember.computed.sort('cards', 'sortedCardsOrder'),
   actions: {
     addCard(card, user, join, count) {
       this.sendAction('addCard', card, user, join, count);
+    },
+    all() {
+      this.set('allCards', true);
+      this.set('classCards', false);
+    },
+    class() {
+      this.set('allCards', false);
+      this.set('classCards', true);
+    },
+    rarity() {
+      this.set('allCards', false);
+      this.set('classCards', false);
+      this.set('rarityCards', true);
+    },
+    set() {
+      this.set('allCards', false);
+      this.set('classCards', false);
+      this.set('rarityCards', false);
+      this.set('setCards', true);
+    },
+    cost() {
+      this.set('allCards', false);
+      this.set('classCards', false);
+      this.set('rarityCards', false);
+      this.set('setCards', false);
+      this.set('costCards', true);
     }
   }
 });
