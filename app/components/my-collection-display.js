e5209b11c2d0a8e63c0e1aed9327b4db1bb70fd3
Lisa Duschek & Kellen Kolbeck <student@epicodus.com>
2015-10-08 09:21:57 -0700

    broken search bar

--- app/components/my-collection-display.js
+++ app/components/my-collection-display.js
@@ -2,6 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   showAsTable: false,
+  searchParam: "",
+  searchTerm: "",
+  filteredCards: Ember.computed('searchParam', 'searchTerm', function() {
+    if(this.get('searchParam') === "") {
+      return this.get('join')
+    } else {
+      return this.get('join').filter(function(cardUser) {
+        if(cardUser.get('card').content.get(this.get('searchParam')) === this.get('searchTerm')) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    }
+  }),
   actions: {
     addCard(card, user, join, count) {
       this.sendAction('addCard', card, user, join, count);
@@ -12,6 +27,9 @@ export default Ember.Component.extend({
       } else {
         this.set('showAsTable', true);
       }
+    },
+    test() {
+      debugger;
     }
   }
 });
