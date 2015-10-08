6ca2995dc795860ddaa509fa0f9a3cedd91c1a43
Christoph Pirringer & Kyle Mellander <student@epicodus.com>
2015-10-08 11:13:48 -0700

    Add search bar for collection

--- app/components/my-collection-display.js
+++ app/components/my-collection-display.js
@@ -2,8 +2,32 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   showAsTable: false,
-  filteredCards: Ember.computed(function() {
-    return this.get('cards'); 
+  cardSearch: "",
+  userCards: Ember.computed('join', function() {
+    var userId = this.get('session').id;
+    return this.get('cards').filter(function(card) {
+      var included = false;
+      card.get('card_users').forEach(function(cardUser){
+        if(cardUser.get('user').id === userId) {
+          included = true;
+        }
+      });
+      return included;
+      // return card.get('name') === "Backstab";
+    });
+  }),
+  filteredCards: Ember.computed('userCards', 'cardSearch', function() {
+    var result = [];
+    var search = this.get('cardSearch').toLowerCase();
+    this.get('userCards').forEach(function(card) {
+      if(card.get('name').toLowerCase().indexOf(search) > -1) {
+        result.push(card);
+      }
+    })
+    return result;
+    // .filter(function(card) {
+    //   card.get('name').indexOf('F') > -1;
+    // })
   }),
   actions: {
     addCard(card, user, join, count) {
