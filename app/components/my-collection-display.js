404385df6011452c27878494c860f88412745034
Christoph Pirringer & Kyle Mellander <student@epicodus.com>
2015-10-08 09:40:37 -0700

    meaningless

--- app/components/my-collection-display.js
+++ app/components/my-collection-display.js
@@ -2,21 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   showAsTable: false,
-  searchParam: "",
-  searchTerm: "",
-  filteredCards: Ember.computed('searchParam', 'searchTerm', function() {
-    if(this.get('searchParam') === "") {
-      return this.get('join')
-    } else {
-      return this.get('join').filter(function(cardUser) {
-        if(cardUser.get('card').content.get(this.get('searchParam')) === this.get('searchTerm')) {
-          return true;
-        } else {
-          return false;
-        }
-      });
-    }
-  }),
+  // searchParam: "name",
+  // searchTerm: "Backstab",
+  // filteredCards: Ember.computed('searchParam', 'searchTerm', function() {
+  //   var self = this;
+  //   if(this.get('searchParam') === "") {
+  //     return this.get('join');
+  //   } else {
+  //     return this.get('join').filter(function(cardUser) {
+  //       if(cardUser.get('card').content.get(self.get('searchParam')) === self.get('searchTerm')) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //     });
+  //   }
+  // }),
   actions: {
     addCard(card, user, join, count) {
       this.sendAction('addCard', card, user, join, count);
